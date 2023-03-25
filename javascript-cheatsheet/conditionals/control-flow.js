7f1d1257154a6a4f20cf6a063e6e971eb6d49e84
raymondnes <raynelreign@gmail.com>
2023-03-25 18:29:12 +0000

    control flows

---
+++ javascript-cheatsheet/conditionals/control-flow.js
@@ -0,0 +1,4 @@
+// 	Control Flow
+
+
+// Control flow is the order in which statements are executed in a program. The default control flow is for statements to be read and executed in order from left-to-right, top-to-bottom in a program file. Control structures such as conditionals ( if statements and the like) alter control flow by only executing blocksof code if certain conditions are met. These structures essentially allow a program to make decisions about which code is executed as the program runs.
