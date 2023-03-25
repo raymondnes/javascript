09490436ee07df166cc19aa2a3aea83a694c60aa
Raymond Toritseju Nesiama <raynesiama@gmail.com>
2023-03-25 14:49:18 +0400

    assignment operator

---
+++ javascript-cheatsheet/Introduction/assignment-operators.js
@@ -0,0 +1,20 @@
+// An assignment operator assigns a value to its left operand based on the value of its right operand. Here are some of them:
+
+
+	let number = 50;
+	number += 10;
+	console.log(number);
+// Prints 60
+
+	number -= 10;
+	console.log(number);
+// Prints 50
+
+	number *= 2;
+	console.log(number);
+// Prints 100
+
+	number /= 5;
+	console.log(number);
+// Prints 20
+
---
+++ javascript-cheatsheet/Introduction/string-interpolation.js
@@ -0,0 +1,10 @@
+// String interpolation is the process of evaluating string literals containing one or more placeholders(expressions, variables, etc). It can be performed using template literals: text ${expression} text .
+	
+	
+// String interpolation
+
+	let age = 7;
+	let childsAge = `Tommy is ${age} years old.`;
+        console.log(childsAge);
+
+// prints : Tommy is 7 years old.
