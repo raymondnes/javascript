b88a31c33942221b474a145a4e02e1a50e777b7d
Raymond Toritseju Nesiama <raynesiama@gmail.com>
2023-03-25 17:27:49 +0400

    for each method

---
+++ javascript-cheatsheet/iterators/for-each_method.js
@@ -0,0 +1,14 @@
+// The .forEach() method executes a callback function on each of the elements in an array in order. In the above example code, the callback function containing a console.log() method will be executed 5 times, once for each element.
+
+
+	const numbers = [28, 77, 45, 99, 27];
+	numbers.forEach(number => {
+	console.log(number);});
+
+// prints: 
+/*28
+  77
+  45
+  99
+  27
+*/
